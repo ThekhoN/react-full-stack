f66c62fc045b7b2d4d75f66da9d3e232c506dbed
ThekhoN <bennetonss@hotmail.com>
2017-09-24 11:20:10 +0530

    use cookie-session

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
-require('./models/User');
-require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 const mongoose = require('mongoose');
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
 const keys = require('./config/keys');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
-authRoutes(app);
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days in ms
+		keys: [keys.cookieKey] // array because it allows to randomly pick one key
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+authRoutes(app); // authRoutes is a function that accepts app as arg
 
 const PORT = process.env.PORT || 5555;
-app.listen(PORT);
+app.listen(PORT, () => {
+	console.log('app running on port: ', PORT);
+});
--- services/passport.js
+++ services/passport.js
@@ -2,7 +2,19 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const mongoose = require('mongoose');
+
 const User = mongoose.model('users');
+// user here refers to the new user created or old user retrieved from db
+passport.serializeUser((user, done) => {
+	done(null, user.id); // user.id refers to unique _id generated in db
+});
+
+// id here refers to _id from db
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(
 	new GoogleStrategy(
