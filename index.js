48e5ce6468b4ace8e0b0261cf5daa63b55efef33
ThekhoN <bennetonss@hotmail.com>
2017-10-12 14:59:17 +0530

    setup emailer client

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
-const billingRoutes = require('./routes/billingRoutes');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-require('./models/User');
-require('./services/passport');
-const keys = require('./config/keys');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
 
+// models
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
+
+// routes
+const billingRoutes = require("./routes/billingRoutes");
+const surveyRoutes = require("./routes/surveyRoutes");
+const authRoutes = require("./routes/authRoutes");
+
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(
   cookieSession({
@@ -21,21 +29,23 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+
 authRoutes(app); // authRoutes is a function that accepts app as arg
 billingRoutes(app);
+surveyRoutes(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Express to serve up production assets by default ~ bundled js & css
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
   // Express serve index.html for non-express handled routes
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
 const PORT = process.env.PORT || 5555;
 app.listen(PORT, () => {
-  console.log('app running on port: ', PORT);
+  console.log("app running on port: ", PORT);
 });
